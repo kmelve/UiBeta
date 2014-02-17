a22a25237e1112f2b1515ea3691b174c57a5f170
kmelve <knut.melvaer@gmail.com>
2014-02-17 19:12:38 +0100

    Some minor tweaks

--- footer.php
+++ footer.php
@@ -9,7 +9,7 @@
     				</div>
 	               
 	                <div class="large-12 medium-12 columns">		
-						<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
+						<p><?php echo date('Y'); ?> <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/deed.en_US"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">UiBeta</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="http://beta.uib.no" property="cc:attributionName" rel="cc:attributionURL">Universitetet i Bergen</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/deed.en_US">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.<br />Based on a work at <a xmlns:dct="http://purl.org/dc/terms/" href="http://beta.uib.no" rel="dct:source">http://beta.uib.no</a>.</p>
 					</div>
 				
 				</div> <!-- end #inner-footer -->
