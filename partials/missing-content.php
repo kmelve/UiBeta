c99d110914a1e22e03aaf70c5126e7e90b3ce6a7
kmelve <knut.melvaer@gmail.com>
2014-02-20 11:45:24 +0100

    Fixed some sidebar styling + updated the 404 page

--- partials/missing-content.php
+++ partials/missing-content.php
@@ -3,6 +3,7 @@
     <h1><?php _e("BOMTUR! Nettsiden eksisterar ikkje.", "jointstheme"); ?></h1>
   </header>
   <section class="entry-content">
+  <img src="http://d.pr/i/qhqQ+" alt="Olsen 404">
     <p><?php _e("Du må dobbeltsjekka om du har fått korrekt nettadresse. Om du kjem i hug noko om innholdet kan du nytte søkeboksen. Lukke til!", "jointstheme"); ?></p>
  </section>
  
