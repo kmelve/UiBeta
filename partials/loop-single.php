a3baf95a15d6cfc1cb27299a0a81baa684d0fd3c
kmelve <knut.melvaer@gmail.com>
2014-02-20 12:54:32 +0100

    Updated 404, added link to header, updated favicons

--- partials/loop-single.php
+++ partials/loop-single.php
@@ -1,7 +1,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 						
 	<header class="article-header">	
-		<h1 class="entry-title single-title" itemprop="headline"><?php the_title(); ?></h1>
+		<h1><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 			<span class="byline"><?php printf(__('Posted by <span class="author label">%3$s</span> on <time class="updated label" datetime="%1$s" pubdate>%2$s</time>. <span class="label">%4$s</span>', 'jointstheme'), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), joints_get_the_author_posts_link(), get_the_category_list(', '));?></span>		
 			<ul class="clearing-thumbs" data-clearing>
 				<li><?php the_post_thumbnail('full'); ?></li>
--- partials/missing-content.php
+++ partials/missing-content.php
@@ -1,9 +1,9 @@
 <article id="post-not-found" class="hentry clearfix">
   <header class="article-header">
-    <h1><?php _e("BOMTUR! Nettsiden eksisterar ikkje.", "jointstheme"); ?></h1>
+    <h1><?php _e("BOMTUR!<br />Ikkje-eksisterande nettside (404).", "jointstheme"); ?></h1>
   </header>
   <section class="entry-content">
-  <img src="http://d.pr/i/qhqQ+" alt="Olsen 404">
+  <img src="http://beta.uib.no/wp-content/uploads/2014/02/uibeta4041.png" alt="Olsen 404">
     <p><?php _e("Du må dobbeltsjekka om du har fått korrekt nettadresse. Om du kjem i hug noko om innholdet kan du nytte søkeboksen. Lukke til!", "jointstheme"); ?></p>
  </section>
  
