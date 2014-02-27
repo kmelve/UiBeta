f09c5995c45039760a1d073d8901c936f96a68c5
kmelve <knut.melvaer@gmail.com>
2014-02-27 11:14:19 +0100

    Added thanks to the footer; because of gratefulness.

--- footer.php
+++ footer.php
@@ -18,9 +18,10 @@
 				<div class="row clearfix">
 					<div class="large-12 medium-12 columns creativecommons">
 						<p><?php echo date('Y'); ?><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/deed.no"><img alt="Creative Commons-lisens" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">UiBeta</span> av <a xmlns:cc="http://creativecommons.org/ns#" href="http://beta.uib.no" property="cc:attributionName" rel="cc:attributionURL">Universitetet i Bergen</a> er lisensiert under en <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/deed.no">Creative Commons Navngivelse-Ikkekommersiell-DelPåSammeVilkår 4.0 Internasjonal Lisens</a>.</p>
+						<p class="thanks">Takk til alle som har bidratt til UiBeta!</p>
 					</div>
 				</div>
-								
+							
 			</footer> <!-- end footer -->
 		
 		</div> <!-- end #container -->
