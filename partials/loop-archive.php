e86615aca6e3e414a8727fb4bcae70948e7d7efe
kmelve <knut.melvaer@gmail.com>
2014-03-10 15:33:56 +0100

    reduced featured image size

--- partials/loop-archive.php
+++ partials/loop-archive.php
@@ -1,11 +1,11 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
 						
 	<header class="article-header">
-		<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+		<h1><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 		
 		<span class="byline"><?php printf(__('Lagt inn av <span class="label author">%3$s</span> den <time class="label updated" datetime="%1$s" pubdate>%2$s</time> i <span class="label">%4$s</span>', 'jointstheme'), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), joints_get_the_author_posts_link(), get_the_category_list(', '));?></span>							
 		<div class="clearfix">
-		<?php the_post_thumbnail('full'); ?>	
+		<a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('medium'); ?></a>
 		</div>
 		
 	</header> <!-- end article header -->
