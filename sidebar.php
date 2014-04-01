5cd4651e8b0032e1acddf810891393d597100812
Knut Melvær <knut.melvaer@gmail.com>
2014-04-01 21:40:57 +0200

    Tilbake til normale fonter + sidebarfix

--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,8 @@
 				<div id="sidebar1" class="sidebar large-4 medium-4 columns" role="complementary">
 
-					<?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
+					<?php if ( is_active_sidebar( 'sidebar-single' ) ) : ?>
 
-						<?php dynamic_sidebar( 'sidebar1' ); ?>
+						<?php dynamic_sidebar( 'sidebar-single' ); ?>
 
 					<?php else : ?>
 
